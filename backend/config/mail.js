d62959f69f26e55b16bd26d0b91b5174471c0f1f
SamriddhiMalhotra <Samriddhimalhotra94@gmail.com>
2026-08-24 10:40:08 +0530

    Update mail.js

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -3,20 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("MAILER FILE LOADED");
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
   },
 });
-
-console.log("EMAIL:", process.env.EMAIL ? "EMAIL EXISTS" : "EMAIL MISSING");
-console.log("PASS:", process.env.PASS ? "PASS EXISTS" : "PASS MISSING");
 
 transporter.verify()
   .then(() => {
@@ -38,6 +34,3 @@ export const sendOtpMail = async (to, otp) => {
     `,
   });
 };
-}
-
-//Your code creates a secure Gmail connection using Nodemailer, then defines a reusable function that takes a user's email and OTP and sends them a password-reset email.
